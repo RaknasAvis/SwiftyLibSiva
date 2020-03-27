f7d303c766cf53319565f52c2b509d94b4a141ce
SIVASANKAR YATHIRAJULA <sivasankar.yathirajula@kofax.com>
2020-03-27 13:24:58 +0530

    Initial project setup

---
+++ SwiftyLibSiva/SwiftyLibSiva.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLibSiva.h
+//  SwiftyLibSiva
+//
+//  Created by SIVASANKAR YATHIRAJULA on 27/03/20.
+//  Copyright © 2020 SIVASANKAR YATHIRAJULA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibSiva.
+FOUNDATION_EXPORT double SwiftyLibSivaVersionNumber;
+
+//! Project version string for SwiftyLibSiva.
+FOUNDATION_EXPORT const unsigned char SwiftyLibSivaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibSiva/PublicHeader.h>
+
+
